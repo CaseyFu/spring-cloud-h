76fcaa8988d36a935b9b392a5ebb9c38cdfec2c6
Fu Kai <ca2pa23187@gmail.com>
2021-05-27 22:45:43 +0800

    ok

--- cloud-account/account-service/src/main/java/org/casey/cloud/h/account/service/CloudAccountServiceApplication.java
+++ cloud-account/account-service/src/main/java/org/casey/cloud/h/account/service/CloudAccountServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackages = {"org.casey.cloud.h.account.service"})
 public class CloudAccountServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CloudAccountServiceApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(CloudAccountServiceApplication.class, args);
+  }
 
 }
